f6c893ac0f76224cb4f13c8c08135dfe19cc1ada
AMAN OJHA <135798421+AMAN-1819@users.noreply.github.com>
2024-07-18 09:41:21 +0530

    Time: 4 ms (79.51%), Space: 9.7 MB (7.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
+           bool isPalindrome(string s) {
         string n;
-        for(int i=0;i<s.size();i++)
-        {if(isalnum(s[i]))
-        {n+=tolower(s[i]);
+        for (char c : s) {
+            if (isalnum(c)) {
+                n += tolower(c);
+            }
         }
-        }
-        string k=n;
-       reverse(k.begin(),k.end());
-        return n==k;
         
+        string k = n;
+        reverse(k.begin(), k.end());
         
+        return n == k;
     }
 };
