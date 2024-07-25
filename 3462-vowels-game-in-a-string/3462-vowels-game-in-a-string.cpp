587fbcbbea866db557753350b12422edac79417b
AMAN OJHA <135798421+AMAN-1819@users.noreply.github.com>
2024-07-25 11:41:39 +0530

    Time: 48 ms (40.69%), Space: 17.4 MB (76.83%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -5,11 +5,7 @@ public:
         for(int i=0;i<s.size();i++)
         {if(s[i]=='a'|| s[i]=='e'||s[i]=='i'|| s[i]=='o'||s[i]=='u')
         count++;}
-        // if(count==0)
-        // {return false;}
-        // else
-        // if(count%2!=0)
-        // {return true;}
+       
         
         return count>=1;
         
