9b7040dd90c43006221a2cf033efd1d36b6f954d
AMAN OJHA <135798421+AMAN-1819@users.noreply.github.com>
2024-09-06 10:34:38 +0530

    Time: 3 ms (29.73%), Space: 7.6 MB (36.43%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int solveMem(int n,vector<int>&dp)
+{if(n==1|| n==0)
+return 1;
+if(dp[n]!=-1)
+{return dp[n];}
+dp[n]=solveMem(n-1,dp)+solveMem(n-2,dp);
+return dp[n];}
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        
+        return solveMem(n,dp);
+    }
+};
